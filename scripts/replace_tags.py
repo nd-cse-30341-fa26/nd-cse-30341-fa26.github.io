d7951700fc72c0502ccaab4e7661e20516316c07
Peter Bui <pbui@nd.edu>
2026-08-24 19:38:47 -0400

    scripts/replace_tags: add support for alias
    Don't insert extra newline at the end

--- scripts/replace_tags.py
+++ scripts/replace_tags.py
@@ -3,21 +3,23 @@
 import re
 import sys
 
-TAGS = (
-    'primary',
-    'success',
-    'caution',
-    'danger',
-    'info',
-    'warning',
-    'special',
-    'muted',
-)
+TAGS = {
+    'primary'   : 'primary',
+    'success'   : 'success',
+    'caution'   : 'caution',
+    'danger'    : 'danger',
+    'info'      : 'info',
+    'warning'   : 'warning',
+    'special'   : 'special',
+    'muted'     : 'muted',
+    'gold'      : 'gold',
+    'comment'   : 'hljs-comment',
+}
 
 text = open(sys.argv[1]).read()
 
-for tag in TAGS:
-    text = re.sub(f'<{tag}>', f'<strong class="{tag}">', text, flags=re.MULTILINE)
+for tag, css_class in TAGS.items():
+    text = re.sub(f'<{tag}>', f'<strong class="{css_class}">', text, flags=re.MULTILINE)
     text = re.sub(f'</{tag}>', '</strong>', text, flags=re.MULTILINE)
 
-print(text)
+print(text, end='')
